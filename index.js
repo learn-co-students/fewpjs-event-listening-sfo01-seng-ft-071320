8149a6cd2a3d3372bb5aeaca37305ab1559c522f
Miqotes <ashleelennab@gmail.com>
2020-08-25 14:52:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    //node that will be listening, stored in input
+    const input = document.getElementById('input');
+    //invoking addEventListener on the node we want to listen
+    // first arument is event name to listen for
+    //second argument function(event) is callback function
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
 }
